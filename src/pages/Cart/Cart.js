0b17d20a23e763bab750186884e2feec8f077eca
Sachin Dhull <sachindhull916@gmail.com>
2023-06-05 10:56:54 +0530

    first commit

---
+++ src/pages/Cart/Cart.js
@@ -0,0 +1,35 @@
+import React, { useContext } from 'react'
+import {PRODUCTS} from '../../products'
+import {ShopContext} from '../../context/shop-context'
+import CartItem from './CartItem'
+import './Cart.css'
+import { useNavigate } from 'react-router-dom'
+const Cart = () => {
+  const {cartItems,getTotalCartAmount} = useContext(ShopContext)
+  const totalAmount = getTotalCartAmount()
+
+  const navigate = useNavigate()
+  return (
+    <div className='cart'>
+      <div><h1>Your Cart Items</h1></div>
+      <div className='cartItems'>
+        {PRODUCTS.map((item)=>{
+          if(cartItems[item.id] !== 0){
+            return<CartItem data={item}/>
+          }
+        })}
+      </div>
+      {totalAmount > 0 ? (
+      <div className='checkout'>
+        <p>Subtotal : ${totalAmount}</p>
+        <button onClick={()=>navigate('/')}>Continue Shopping</button>
+        <button>Checkout</button>
+      </div>):(
+        <h1>Your Cart is Empty!</h1>
+      )}
+   
+    </div>
+  )
+}
+
+export default Cart
